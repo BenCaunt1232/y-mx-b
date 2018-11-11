70f557c0bcd1a1209778770c2eee7882725ce3bc
BenCaunt1232 <44347192+BenCaunt1232@users.noreply.github.com>
2018-11-11 13:53:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-
-#takes user input
-#the 'int' makes every input an integer as apposed to a String (strings are simply text that cannot be used in math )
-y1 = int(input('y1: '))
 x1 = int(input('x1: '))
-y2 = int(input('y2: '))
+y1 = int(input('y1: '))
 x2 = int(input('x2: '))
+y2 = int(input('y2: '))
 
-#normally you would just use one varible for this but i used 3 simply for compatability with older versions of python
+#slope intercept form (y=mx+b)
 SlopeY = y2-y1
 print("SlopeY: ")
 print(SlopeY)
 SlopeX = x2-x1
 print("SlopeX: ")
 print(SlopeX)
-#the final step to solving for slope and
 Slope = SlopeY/SlopeX
-#since we are solving for slope we are putting the b on the left side and letting python work its magic
 b = y1-(Slope*x1)
-print(b)
+print("y-intercept: ",b)
 print("equation:  y =","(",SlopeY,"/",SlopeX,")X+", b)
 
+#mid point
+mid_x = x2-x1
+mid_y = y2-y1
+print('midpoint:  ',  '(' , mid_x, '/2,' , mid_y, '/2 )' )
